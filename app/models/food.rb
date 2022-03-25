31844c42332fc5896a7f9da49ae4de650b029dfd
PeterYves <iradupeteryves@gmail.com>
2022-03-25 17:39:59 +0200

    added all necessary associations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,8 @@
 class Food < ApplicationRecord
+  belongs_to :shop
+  has_many :order_foods, dependent: :destroy
+  has_many :orders, through: :order_foods, dependent: :destroy
+  has_many :italian_foods
+  has_many :japanese_foods
+  has_many :chinese_foods
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,7 @@
 class Shop < ApplicationRecord
+  has_many :addresses, as: :addressable, dependent: :destroy
+  has_many :foods, dependent: :destroy
+  has_many :italian_foods, :through => :foods
+  has_many :japanese_foods, :through => :foods
+  has_many :chinese_foods, :through => :foods
 end
