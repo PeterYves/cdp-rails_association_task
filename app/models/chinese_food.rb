54798e81ddece8e7cc549576db30effc1c5b7e30
PeterYves <iradupeteryves@gmail.com>
2022-03-25 22:00:23 +0200

    fixind foods models associations

--- app/models/chinese_food.rb
+++ app/models/chinese_food.rb
@@ -1,5 +1,5 @@
 class ChineseFood < ActiveRecord::Base
   belongs_to :shop
-  has_many :order_foods
-  has_many :orders, through: :order_foods
+  has_many :order_foods, dependent: :destroy
+  has_many :orders, through: :order_foods, dependent: :destroy
 end
