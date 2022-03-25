db4f77e646412b3b79703123505b44c5417f4cb7
PeterYves <iradupeteryves@gmail.com>
2022-03-25 18:42:30 +0200

    modified food models

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,4 +5,7 @@ class Food < ApplicationRecord
   has_many :italian_foods, dependent: :destroy
   has_many :japanese_foods, dependent: :destroy
   has_many :chinese_foods, dependent: :destroy
+  has_many :foods, through: :japanese_foods, dependent: :destroy
+  has_many :foods, through: :italian_foods, dependent: :destroy
+  has_many :foods, through: :chinese_foods, dependent: :destroy
 end
